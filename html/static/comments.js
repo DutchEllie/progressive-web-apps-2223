f5883b287d926b26ccdeba7e98793978469ca75d
DutchEllie <personal@quenten.nl>
2023-03-27 08:56:53 +0200

    Fixed JS

--- html/static/comments.js
+++ html/static/comments.js
@@ -5,8 +5,8 @@ let currentPage = 1;
 // Initial value, gets changed in nextButton's EventListener
 let maxPage = -1;
 
-const previousLink = document.querySelector(".guestbook-comments > a:first-of-type");
-const nextLink = document.querySelector(".guestbook-comments > a:nth-of-type(2)");
+const previousLink = document.querySelector(".guestbook > a:first-of-type");
+const nextLink = document.querySelector(".guestbook > a:nth-of-type(2)");
 
 previousLink.addEventListener("click", async(event) =>  {
 	event.preventDefault();
@@ -16,112 +16,24 @@ previousLink.addEventListener("click", async(event) =>  {
 	await setComments(url);
 	
 	return false;
-	// if (currentPage == 1) {
-	// 	return
-	// } else {
-	// 	currentPage--;
-	// }
-	// const comments = await loadComments(pageSize, currentPage);
-
-	// const guestbookCommentsArticles = document.querySelectorAll(".guestbook-comments > article");
-	// const guestbookCommentsWrapper = document.querySelector(".guestbook-comments");
-	// // Remove them all
-	// if(guestbookCommentsArticles.length > 0){
-	// 	guestbookCommentsArticles.forEach(element => {
-	// 		guestbookCommentsWrapper.removeChild(element);
-	// 	});
-	// }
-	
-	// for (let i = 0; i < pageSize ; i++) {
-	// 	let gbArticle = document.createElement("article");
-	// 	let gbHeader = document.createElement("header");
-	// 	let gbAuthor = document.createElement("p");
-	// 	let gbTimeField = document.createElement("p");
-	// 	let gbTime = document.createElement("time");
-	// 	let gbMessage = document.createElement("p");
-
-	// 	gbAuthor.textContent = "Author: " + comments[i].name;
-	// 	gbTimeField.textContent = "Time: ";
-	// 	gbTime.textContent = comments[i].time;
-	// 	gbTimeField.appendChild(gbTime);
-	// 	// TODO: Parse datetime and put it in here
-	// 	//gbTime.dateTime = 
-	// 	gbMessage.textContent = comments[i].message;
-	// 	gbHeader.appendChild(gbAuthor);
-	// 	gbHeader.appendChild(gbTimeField);
-	// 	gbArticle.appendChild(gbHeader);
-	// 	gbArticle.appendChild(gbMessage);
-
-	// 	guestbookCommentsWrapper.appendChild(gbArticle);
-	// }
-	
 }, false);
 
 nextLink.addEventListener("click", async(event) => {
-	event.preventDefault;
+	event.preventDefault();
 	console.log("PAIN");
 	
 	let url = nextLink.getAttribute('href');
 	await setComments(url);
 
 	return false;
-	// currentPage++;
-	
-	// const comments = await loadComments(pageSize, currentPage);
-
-	// // If we get fewer comments back than the pageSize, we know we've reached the end.
-	// // So now we set the maxPage to that size and we know what the max is.
-	// if(comments.length < pageSize) {
-	// 	maxPage = currentPage;
-	// } 
-
-	// // If maxPage is not the initial value and it's equal to the currentPage,
-	// // prevent incrementing!
-	// if (maxPage > -1 && maxPage == currentPage) {
-	// 	// Reset what we did above, when we incremented it.
-	// 	currentPage--;
-	// 	return
-	// }
-	
-	// const guestbookCommentsArticles = document.querySelectorAll(".guestbook-comments > article");
-	// const guestbookCommentsWrapper = document.querySelector(".guestbook-comments");
-	// // Remove them all
-	// if(guestbookCommentsArticles.length > 0){
-	// 	guestbookCommentsArticles.forEach(element => {
-	// 		guestbookCommentsWrapper.removeChild(element);
-	// 	});
-	// }
-	
-	// for (let i = 0; i < pageSize ; i++) {
-	// 	let gbArticle = document.createElement("article");
-	// 	let gbHeader = document.createElement("header");
-	// 	let gbAuthor = document.createElement("p");
-	// 	let gbTimeField = document.createElement("p");
-	// 	let gbTime = document.createElement("time");
-	// 	let gbMessage = document.createElement("p");
-
-	// 	gbAuthor.textContent = "Author: " + comments[i].name;
-	// 	gbTimeField.textContent = "Time: ";
-	// 	gbTime.textContent = comments[i].time;
-	// 	gbTimeField.appendChild(gbTime);
-	// 	// TODO: Parse datetime and put it in here
-	// 	//gbTime.dateTime = 
-	// 	gbMessage.textContent = comments[i].message;
-	// 	gbHeader.appendChild(gbAuthor);
-	// 	gbHeader.appendChild(gbTimeField);
-	// 	gbArticle.appendChild(gbHeader);
-	// 	gbArticle.appendChild(gbMessage);
-
-	// 	guestbookCommentsWrapper.appendChild(gbArticle);
-	// }
-	
 }, false);
 
 async function setComments(url) {
-	// let URLPain = new URL(url);
-	// URLPain.searchParams.set('call', 'javascript');
-
-	let resp = await fetch(url + "/?call=javascript");
+	let resp = await fetch(url, {
+		headers: {
+			callsource: "js"
+		}
+	});
 
 	let data = await resp.text();
 	
--- src/hbs.rs
+++ src/hbs.rs
@@ -1,8 +1,54 @@
+use std::borrow::Cow;
+
 use crate::elements;
 use crate::Redis;
 use crate::elements::Comments;
+use rocket::http::ContentType;
+use rocket::http::hyper::server::Server;
+use rocket::request::{self, Request, Outcome, FromRequest};
 use rocket_db_pools::Connection;
+use rocket_dyn_templates::handlebars::template;
 use rocket_dyn_templates::{context, Template};
+use rocket::http::Status;
+use rocket::response::{self, Responder, Response, Result};
+use rocket::http::Header;
+
+// #[derive(rocket::Responder)]
+// struct ServerTimingResponder<'a> {
+// 	inner: Template,
+// 	header: ContentType,
+// 	server_timing: Header<'static>,
+
+// 	response: Response<'a>
+// }
+
+// impl<'r, 'a> Responder<'r, 'static> for ServerTimingResponder<'a> {
+// 	fn respond_to(self, request: &'r Request<'_>) -> Result<'static> {
+// 		Ok(self.response)
+// 	}
+// }
+
+// impl <'a> ServerTimingResponder<'a> {
+// 	pub fn add_metric(self, n: &str, v: &str) {
+// 		let old = self.server_timing.value().clone().to_owned();
+// 		self.server_timing.value = Cow(old + n + ";" + v);
+// 	}
+// }
+
+
+pub struct Data(String);
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for Data {
+	type Error = ();
+
+	async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+		match req.headers().get_one("callsource") {
+			Some(val) => { Outcome::Success(Data(val.to_string())) },
+			None => { Outcome::Success(Data("not-js".to_string())) }
+		}
+	}
+}
 
 #[get("/")]
 pub async fn index(mut db: Connection<Redis>) -> Template {
@@ -20,11 +66,18 @@ pub async fn index(mut db: Connection<Redis>) -> Template {
 		previous_page: 1,
 		next_page: 2,
 	};
+
+	// let response = ServerTimingResponder {
+	// 	inner: Template::render("index", &context),
+	// 	header: rocket::http::ContentType::parse_flexible("html").unwrap(),
+	// 	pain: Header::new("oof", "oof")
+	// };
+	// return response;
 	Template::render("index", &context)
 }
 
 #[get("/comments/<commentpage>")]
-pub async fn index_comments(commentpage: u32, mut db: Connection<Redis>) -> Template {
+pub async fn index_comments(commentpage: u32, data: Data, mut db: Connection<Redis>) -> Template {
 	let res = get_comments(commentpage, db).await;
 	let page_size = 10;
 
@@ -53,7 +106,16 @@ pub async fn index_comments(commentpage: u32, mut db: Connection<Redis>) -> Temp
 		next_page: next_page
 	};
 
-	Template::render("index", &context)
+	return match data.0.as_str() {
+		"not-js" => {
+			Template::render("index", &context)
+		}
+		_ => {
+			Template::render("comments", &context)
+		}
+	}
+
+	// Template::render("index", &context)
 }
 
 #[get("/comments/<commentpage>?<call>")]
